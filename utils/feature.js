b50da202756e9c710e8e90ce065f61796b404dfd
Abhishek <ikharabhishek177@gmail.com>
2023-11-12 11:25:12 +0530

    Make secure true if it is production

--- utils/feature.js
+++ utils/feature.js
@@ -11,8 +11,7 @@ export const sendCookie = async (user,res,message,statusCode=200) =>{
         httpOnly: true,
         maxAge  : 15*60*1000,
         samesite: process.env.NODE_ENV === "Development" ? "lax":'none', // because our frontend and backend will be at different url
-        // secure:process.env.NODE_ENV === "Development" ? false:true,
-        secure:false,
+        secure:process.env.NODE_ENV === "Development" ? false:true,
     }
     ).json(
         {
